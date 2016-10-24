f6cc6bd55bce2d473ff312f26a974b1b1d7b2e6e
OopsLOL <glumfree@gmail.com>
2016-10-24 19:27:39 +0300

    Update trans.js

--- trans.js
+++ trans.js
@@ -1,3 +1,4 @@
+
 var title = document.head.querySelector('title').innerHTML;
 if (title == 'Change Password - Play free online games') {
 	document.title = 'Изменить пароль';
@@ -67,7 +68,7 @@ if (title == 'Ticket Lists - Play free online games') {
          "Sign Up"                                 : "Регистрация",
          "Log in with"                             : "Войти с помощью",
          "Log In"                                  : "Войти",
-         "Quick"                                   : "Быстрая",
+         //"Quick"                                   : "Быстрая",
          "Sign up for news & early access to games": " Подписаться на новости.",
          "I accept the"                            : "Я принимаю",
          "Terms of Service"                        : "Условия предоставления услуг",
@@ -80,15 +81,15 @@ if (title == 'Ticket Lists - Play free online games') {
          "Please enter your password"              : "Введите пароль",
          "Sorry, account does not exist"           : "Пользователь не найден",
          "Sorry, wrong password"                   : "Неверный пароль",
-         "Old"                                     : "Старый",
-         "New"                                     : "Новый",
-         "Confirm"                                 : "Подтвердить",
+         //"Old"                                     : "Старый",
+         //"New"                                     : "Новый",
+         //"Confirm"                                 : "Подтвердить",
          "Confirm password"                        : "Подтвердить пароль",
          "Verification"                            : "Проверочный",
          "SUBJECT"                                 : "Тема",
          "DATE"                                    : "Дата",
          "STATUS"                                  : "Статус",
-         "Game"                                    : "Игра",
+         //"Game"                                    : "Игра",
          "Server"                                  : "Сервер",
          "Records"                                 : "Записи",
          "Date"                                    : "Дата",
@@ -101,8 +102,8 @@ if (title == 'Ticket Lists - Play free online games') {
          "First Name"                              : "Имя",
          "Last Name"                               : "Фамилия",
          "Gender"                                  : "Пол",
-         "Male"                                    : "Мужской",
-         "Female"                                  : "Женский",
+         "Male"                                    : " Мужской",
+         "Female"                                  : " Женский",
          "Address"                                 : "Адрес",
          "User Name:"                              : "Пользователь:",
          "SELECT PAYMENT METHOD"                   : "ВЫБЕРИТЕ СПОСОБ ОПЛАТЫ",
@@ -113,7 +114,7 @@ if (title == 'Ticket Lists - Play free online games') {
          "SERVER"                                  : "СЕРВЕР",
          "RECHARGE AMOUNT"                         : "СУММА ПОПОЛНЕНИЯ",
          "PURCHASED ITEMS"                         : "ПРИОБРЕТЕННЫЕ",
-         "Gildz"                                   : "Золотых",
+         //"Gildz"                                   : "Золотых",
          "USD"                                     : "доллар(ов)",
          "Order Id"                                : "Транзакция",
          "Amount"                                  : "Сумма",
@@ -126,9 +127,9 @@ if (title == 'Ticket Lists - Play free online games') {
          "Payment"                                 : "Оплата",
          "Other"                                   : "Другое",
          "Please select a category"                : "Выберите категорию",
-         "Please select your Character Name"       : "Выберите игровой ник",
+         //"Please select your Character Name"       : "Выберите игровой ник",
          "Mobile"                                  : "Мобильный",
-         "Security E-mail"                         : "Почтовый ящик(указывается при регистрации,нельзя изменить)",
+         "Security E-mail"                         : "Почтовый ящик (указывается при регистрации,нельзя изменить)",
          "Security Question 1"                     : "Секретный вопрос №1",
          "Security Question 2"                     : "Секретный вопрос №2",
          "Answer1"                                 : "Ответ на секретный вопрос №1",
@@ -138,6 +139,7 @@ if (title == 'Ticket Lists - Play free online games') {
          "What is my favourite movie"              : "Любимый фильм",
          "Where was my father born"                : "Место рождения отца",
          "What is my mother's birthday"            : "День рождения матери",
+         "Who is the person I like most"           : "Мой кумир",
          "Old Password"                            : "Старый пароль",
          "New Password"                            : "Новый пароль",
          "Confirm Password"                        : "Подтвердите пароль",
@@ -148,7 +150,7 @@ if (title == 'Ticket Lists - Play free online games') {
          "View Case"                               : "Просмотр тикета",
          "Case list"                               : "Тикеты",
          "Replied"                                 : "Ответили",
-	 "Closed"                                  : "Закрыт",
+         "Closed"                                  : "Закрыт",
          "Pending"                                 : "В ожидании",
          "Account"                                 : "Аккаунт",
          "My Profile"                              : "Мой профиль",
@@ -166,13 +168,16 @@ if (title == 'Ticket Lists - Play free online games') {
          "- How helpful was my reply?"             : "- Насколько полезен ответ ",
          "- Your Rating: "                         : "- Ваша оценка: ",
          "CONFIRM"                                 : "ПОДТВЕРДИТЬ",
-         "Description*"                            : "Описание",
+         //"Description*"                            : "Описание",
          "Screenshot"                              : "Скриншот",
          "Please take a screenshot of your problem and upload it so we can process your case faster." : "Пожалуйста, загрузите скриншот проблемы, чтобы мы могли обработать ваш тикет быстрее.",
          "DEL"                                     : "УДАЛИТЬ",
          "You must select the gift type, game and server above!" : "Вы должны выбрать тип подарка, игру и сервер выше!",
          "Each pack can only be taken once per character"        : "Каждый подарок может быть получен только один раз",
          "Warm Tips:"                              : "Внимание:",
+         "Newbie Pack"                             : "Подарок",
+         "This is a 490 Gildz giftpack for the new palyers" : "Подарочный набор на 490 золотых для новых игроков",
+         "Password"                                : "Пароль",
         };
         for(var t in ts) {
             findAndReplace(t,ts[t]);
@@ -182,21 +187,78 @@ if (title == 'Ticket Lists - Play free online games') {
     setTimeout(translate, 0);
 })();
 
-       document.getElementsByName('oldPassword')[0].placeholder = 'Введите старый пароль';
-       document.getElementsByName('newPassword')[0].placeholder = 'Введите новый пароль';
-       document.getElementsByName('newPassword2')[0].placeholder = 'Введите повторно новый пароль';
-       //document.querySelector('label[for=\"register-password\"]').textContent = 'Пароль'; 
+if (window.location.hostname + window.location.pathname === "user.gogogame.com/Info/changepassword"){
+       document.querySelector('input#oldPassword').placeholder = 'Введите старый пароль';
+       document.querySelector('input#newPassword').placeholder = 'Введите новый пароль';
+       document.querySelector('input#newPassword2').placeholder = 'Введите повторно новый пароль';
 
-	document.getElementById('reg_account').placeholder = 'Укажите почтовый адрес';
-	document.getElementById('user_name').placeholder = 'Укажите почтовый адрес';
-	document.getElementById('user_pwd').placeholder = 'Введите пароль';
-	document.getElementById('reg_pwd').placeholder = 'Введите пароль';
-	document.getElementById('reg_pwd2').placeholder = 'Подтвердите пароль';
-	document.getElementById('reg_verify').placeholder = 'Код';
-	document.getElementById('email').placeholder = 'Укажите ваш@почтовый.ящик';
-	document.getElementById('subject').placeholder = 'Укажите тему сообщения';
-	document.getElementById('message').placeholder = 'Пожалуйста, укажите как можно больше деталей, чтобы мы могли лучше помочь вам!';
+   if (document.querySelector('div.small_title').innerHTML === 'Old Password'){
+       document.querySelector('div.small_title').innerHTML = 'Старый пароль';
+      }
 
-	document.querySelector('span#role_name_tip').innerHTML = 'Выберите игрового персонажа';
-        document.getElementById('del').title = 'Удалить изображение';
-	document.getElementById('rate_message').placeholder = 'Пожалуйста, оставьте комментарий о качестве поддержки (только английский).';
+   if (document.querySelector('div.small_title').innerHTML === 'New Password'){
+       document.querySelector('div.small_title').innerHTML = 'Новый пароль';
+      }
+
+   if (document.querySelector('div.small_title').innerHTML === 'Confirm Password'){
+       document.querySelector('div.small_title').innerHTML = 'Подтвердить пароль';
+      }
+}
+
+if (window.location.hostname === 'pay.gogogame.com') {
+    if (document.querySelector('span.txts').innerHTML === 'GLOBAL') {   
+     document.querySelector('span.txts').innerHTML = 'Общий';
+    }
+    if (document.querySelector('span.notice-wrap.fr').innerHTML === 'Payment Region'){
+       document.querySelector('span.notice-wrap.fr').innerHTML = 'Регион оплаты';
+    }
+}
+
+if (window.location.hostname + window.location.pathname === 'user.gogogame.com/gifts'){
+   if (document.querySelector('span.input-name.fl').innerHTML === 'Game'){
+      document.querySelector('span.input-name.fl').innerHTML = 'Игра';
+     }
+   if (document.querySelector('.seb_list dl dt').innerHTML === 'Item Description'){
+      document.querySelector('.seb_list dl dt').innerHTML = 'Описание';  
+   }
+   document.querySelector('a#collect.btn-normal.btn-orange').innerHTML = 'Запросить';
+}
+
+if (window.location.hostname + window.location.pathname === 'user.gogogame.com/ticket/create'){
+   if (document.querySelector('label.small_title').innerHTML === 'Game*'){
+       document.querySelector('label.small_title').innerHTML = 'Игра*';
+      }
+   if (document.querySelector('.content .right dl dd .small_title').innerHTML === 'Description*'){
+       document.querySelector('.content .right dl dd .small_title').innerHTML = 'Описание*';
+      }
+      document.querySelector('a#ticket_sub.btn-normal.btn-orange').innerHTML = 'Отправить';
+   
+       document.getElementById('reg_account').placeholder = 'Укажите почтовый адрес';
+       document.getElementById('user_name').placeholder = 'Укажите почтовый адрес';
+       document.getElementById('user_pwd').placeholder = 'Введите пароль';
+       document.getElementById('reg_pwd').placeholder = 'Введите пароль';
+       document.getElementById('reg_pwd2').placeholder = 'Подтвердите пароль';
+       document.getElementById('reg_verify').placeholder = 'Код';
+       document.getElementById('email').placeholder = 'Укажите ваш@почтовый.ящик';
+       document.getElementById('subject').placeholder = 'Укажите тему сообщения';
+       document.getElementById('message').placeholder = 'Пожалуйста, укажите как можно больше деталей, чтобы мы могли лучше помочь вам!';  
+       document.querySelector('span#role_name_tip').innerHTML = 'Выберите игрового персонажа';
+       document.getElementById('del').title = 'Удалить изображение';
+       document.getElementById('rate_message').placeholder = 'Пожалуйста, оставьте комментарий о качестве поддержки (только английский).';
+ }
+
+       document.getElementById('reg_account').placeholder = 'Укажите@почтовый.адрес';
+       document.getElementById('user_name').placeholder = 'Укажите@почтовый.адрес';
+       document.getElementById('user_pwd').placeholder = 'Введите пароль';
+       document.getElementById('reg_pwd').placeholder = 'Введите пароль';
+       document.getElementById('reg_pwd2').placeholder = 'Подтвердите пароль';
+       document.getElementById('reg_verify').placeholder = 'Код';
+       document.getElementById('email').placeholder = 'Укажите ваш@почтовый.ящик';
+       //if (document.querySelector('label[for="register-password"]').innerHTML === 'Password'){
+       //document.querySelector('.column label').textContent = 'Пароль';
+       //}
+       document.querySelector('label[for="register-email"]').textContent = 'Почтовый ящик';
+
+       /*document.querySelector('input[name="city"]').placeholder = 'Город'; 
+       document.querySelector('input[name="state"]').placeholder = 'Штат'; 
+       document.querySelector('input[name="country"]').placeholder = 'Страна'; */
